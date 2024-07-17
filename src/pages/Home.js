0ef418be34a23b5d93ce122809b4fc16aa20e881
Hannah Kim <xhannahkim1@gmail.com>
2024-07-16 19:16:57 -0700

    home page (no nav bar)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,48 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import Spline from '@splinetool/react-spline';
+
+
+
 function App() {
     return (
-        <div>
-            <h1>home</h1>
-            <Link to="/AboutUs">About Us</Link>
+        <div style={styles.body}>
+            
+            <Spline scene="https://prod.spline.design/vlBdDrCLuKb271ed/scene.splinecode" />
+            <div style={styles.info}>
+                <p style={styles.pg1}>We are a full service management company who is dedicated in providing the gold standard in management services.</p>
+                <p style={styles.pg2}>Our ambition is to create a property management solution that is customized to assist residential, commercial, and multi family properties including condominum, townhomes, apartment buildings, and Home Owner's Associations.</p>
+            </div>
+
         </div>
     );
+}
+
+const styles = {
+    body: {
+        backgroundColor: '#5C5C5C',
+    },
+
+    info: {
+        backgroundColor: '#fff',
+        borderRadius: 25,
+        padding: 10,
+        display: 'flex',
+        marginLeft: 50,
+        marginRight: 50,
+    },
+
+    pg1: {
+        textAlign: 'right',
+        marginRight: 15,
+    },
+
+    pg2: {
+        textAlign: 'left',
+        marginLeft: 15,
+
+    },
 };
 
 export default App;
