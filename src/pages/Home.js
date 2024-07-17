7fb93f3b63e2dbd08d8e5b80aa5814af8d936e79
Hannah Kim <xhannahkim1@gmail.com>
2024-07-16 19:22:30 -0700

    navbar connected to all

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+//import Navbar from '../components/layout/Navbar';
 import Spline from '@splinetool/react-spline';
 
 
--- src/pages/Owner.js
+++ src/pages/Owner.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from '../components/layout/NavBar';
+import Navbar from '../components/layout/Navbar';
 
 function Owner() {
     return (
