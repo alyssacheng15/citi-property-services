71b6b3348255c3db60526fad510bcb805b3070c0
Alyssa Cheng <alysaycheng15@gmail.com>
2024-07-23 16:53:34 -0700

    about us template

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from '../components/layout/Navbar';
+import Spline from '@splinetool/react-spline';
+import styles from './AboutUs.module.css';
 
-function AboutUs() {
-    return (
-        <div>
-            <Navbar />
-            <h1>about</h1>
-        </div>
-    );
+const AboutUs = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.panel}>Panel 1</div>
+      <div className={styles.panel}>Panel 2</div>
+      <div className={styles.panel}>Panel 3</div>
+      <div className={styles.panel}>Panel 4</div>
+      <div className={styles.panel}>Panel 5</div>
+    </div>
+  );
 };
 
 export default AboutUs;
