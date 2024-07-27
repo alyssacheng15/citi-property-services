5612ba75dd61e58b6c31be5a70751ee1f6c8d534
Alyssa Cheng <alysaycheng15@gmail.com>
2024-07-26 18:47:32 -0700

    flex panels

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -4,6 +4,8 @@ import Navbar from '../components/layout/Navbar';
 import Spline from '@splinetool/react-spline';
 import styles from './AboutUs.module.css';
 import aboutUsHeader from '../assets/images/aboutUsHeader.png';
+import building1 from '../assets/images/building1.png';
+import building2 from '../assets/images/building2.png';
 
 
 const AboutUs = () => {
@@ -16,11 +18,15 @@ const AboutUs = () => {
         <img src={aboutUsHeader} className={styles.aboutUsHeader} alt="header" />
       </div>
       
-      <div className={styles.panel}>Panel 1</div>
-      <div className={styles.panel}>Panel 2</div>
-      <div className={styles.panel}>Panel 3</div>
-      <div className={styles.panel}>Panel 4</div>
-      <div className={styles.panel}>Panel 5</div>
+      <div className={styles.panel}>Citi Property Services, Inc is a Los Angeles, CA based company that leverages the talent of its experienced Managing Principal and their well-developed network of professionals.</div>
+      <div className={styles.panel}>
+        <img src={building1} className={styles.imgpanel}></img>
+      </div>
+      <div className={styles.panel}>Citi Property Services has proven experience in offering a staff that is committed, focused, and sensitive to quality workmanship, protecting and maximizing the value of the asset, on-time performance, job site security, and competitive pricing.</div>
+      <div className={styles.panel}>
+        <img src={building2} className={styles.imgpanel}></img>
+      </div>
+      <div className={styles.panel}>We are dedicated to efficiently managing your property, whether it is a residential rental, commercial strip center, condo association, or apartment complex.</div>
     </div>
     </div>
 
